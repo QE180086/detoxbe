5f117264a6b1ad25094df352f480c9129e376da7
QE180086 <145005942+QE180086@users.noreply.github.com>
2025-10-28 15:03:41 +0700

    base chat service

--- src/main/java/com/parttime/job/Application/projectmanagementservice/chatmanagement/service/ChatService.java
+++ src/main/java/com/parttime/job/Application/projectmanagementservice/chatmanagement/service/ChatService.java
@@ -1,4 +1,14 @@
 package com.parttime.job.Application.projectmanagementservice.chatmanagement.service;
 
+import com.parttime.job.Application.common.request.PagingRequest;
+import com.parttime.job.Application.common.response.PagingResponse;
+import com.parttime.job.Application.projectmanagementservice.chatmanagement.entity.Message;
+import com.parttime.job.Application.projectmanagementservice.chatmanagement.response.MessageResponse;
+
 public interface ChatService {
+    Message sendMessage(String roomId, String senderId, String text, String imageUrl);
+
+    void markAsSeen(String messageId, String userId);
+
+    PagingResponse<MessageResponse> getMessages(String roomId, PagingRequest pagingRequest);
 }
--- src/main/java/com/parttime/job/Application/projectmanagementservice/chatmanagement/service/impl/ChatServiceImpl.java
+++ src/main/java/com/parttime/job/Application/projectmanagementservice/chatmanagement/service/impl/ChatServiceImpl.java
@@ -1,4 +1,38 @@
 package com.parttime.job.Application.projectmanagementservice.chatmanagement.service.impl;
 
-public class ChatServiceImpl {
+import com.parttime.job.Application.common.request.PagingRequest;
+import com.parttime.job.Application.common.response.PagingResponse;
+import com.parttime.job.Application.projectmanagementservice.chatmanagement.entity.Message;
+import com.parttime.job.Application.projectmanagementservice.chatmanagement.repository.MessageRepository;
+import com.parttime.job.Application.projectmanagementservice.chatmanagement.repository.RoomRepository;
+import com.parttime.job.Application.projectmanagementservice.chatmanagement.response.MessageResponse;
+import com.parttime.job.Application.projectmanagementservice.chatmanagement.service.ChatService;
+import com.parttime.job.Application.projectmanagementservice.usermanagement.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ChatServiceImpl implements ChatService {
+    private final MessageRepository messageRepository;
+    private final RoomRepository roomRepository;
+    private final UserRepository userRepository;
+    private final SimpMessagingTemplate messagingTemplate;
+    private final ChatCacheService cacheService;
+
+    @Override
+    public Message sendMessage(String roomId, String senderId, String text, String imageUrl) {
+        return null;
+    }
+
+    @Override
+    public void markAsSeen(String messageId, String userId) {
+
+    }
+
+    @Override
+    public PagingResponse<MessageResponse> getMessages(String roomId, PagingRequest pagingRequest) {
+        return null;
+    }
 }
